e4a6e4593d67ff2ad85a86356cc0af2b74e6ea7d
vytautas.mackonis <vytautas.mackonis@gmail.com>
2019-10-14 05:59:38 +0300

    different way of making sure newlines are substituted

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-echo "${SONATYPE_PRIVATE_KEY@E}" > private.asc
+echo -e $SONATYPE_PRIVATE_KEY > private.asc
 docker-compose run --rm -e SONATYPE_USER=$SONATYPE_USER -e SONATYPE_PASSWORD=$SONATYPE_PASSWORD build mvn deploy
